d3c3826044c14a812f784bf14126b3a9800d499f
Nimor111 <georgi.bojinov@hotmail.com>
2017-03-28 16:29:44 +0300

    Try another fix

--- fullblog/urls.py
+++ fullblog/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+import .settings
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls', namespace='blog')),
 ]
+
+urlpatterns += patterns(
+    '',
+    (r'^static/(?P<path>.*)$',
+     'django.views.static.serve',
+     {'document_root': settings.STATIC_ROOT}),
+)
