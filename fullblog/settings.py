c34744aedca734813234e73534bb97a569e0c710
Nimor111 <georgi.bojinov@hotmail.com>
2017-03-30 15:56:04 +0300

    Add login/logout/regs

--- fullblog/settings.py
+++ fullblog/settings.py
@@ -124,4 +124,5 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+LOGIN_REDIRECT_URL = '/'
--- fullblog/urls.py
+++ fullblog/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.contrib.auth import views as auth_views
+from blog.views import register_view
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls', namespace='blog')),
+    url(r'^', include('blog.urls', namespace='blog')),
+    url(r'^login/$', auth_views.login, {'template_name': 'website/page-user-login-classic.html'},
+        name='login'),
+    url(r'logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'register/$', register_view, name='register'),
 ]
