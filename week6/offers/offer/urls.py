f9bf9fe3254e985abecf8e3424e263c978b20fbc
Nimor111 <georgi.bojinov@hotmail.com>
2017-04-05 20:47:14 +0300

    Submenu in base, appears on all pages now, search category started

--- week6/offers/offer/urls.py
+++ week6/offers/offer/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index_view, name='index'),
     url(r'^add-offer', views.offer_create_view, name='offer-create'),
+    url(r'^category/(?P<pk>[0-9]+)/$', views.offer_category_view, name='category'),
 ]
--- week6/offers/offer/views.py
+++ week6/offers/offer/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 
-from .models import Offer
+from .models import Offer, Category
 from .forms import OfferModelForm
 
 
-# Create your views here.
 def index_view(request):
     offers = Offer.objects.select_related('category', 'author').all()
 
@@ -22,3 +21,10 @@ def offer_create_view(request):
             return redirect(reverse_lazy('offer:index'))
 
     return render(request, 'website/add_offer.html', locals())
+
+
+def offer_category_view(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    offers = Offer.objects.filter(category=category)
+
+    return render(request, 'website/offers_by_category.html', locals())
